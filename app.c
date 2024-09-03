c704e069944748c5f714254124b4f7808fc95ccc
Naveen D <144983660+NAVEENKUMARD16@users.noreply.github.com>
2024-09-03 14:41:02 +0530

    Add files via upload

---
+++ app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"mymath.h"
+int main()
+{
+	int(no1=32,no2=8,result;
+	result=sum(no1,no2);
+	printf("sum of two numbers is %d\n",result);
+	result=diff(no1,no2);
+	printf("diff of two numbers is %d\n",result);
+	return 0;
+}
+
